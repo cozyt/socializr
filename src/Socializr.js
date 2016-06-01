f27511b8c0d974c0a3c3f2a064a50176680a4f97
Tony Harvey <anthony.h@haygarth.co.uk>
2016-06-01 16:17:19 +0100

    Add support for pinterest

--- src/Socializr.js
+++ src/Socializr.js
@@ -51,6 +51,16 @@ Socializr.loadSdk = function(platform) {
 
         case 'google-plus':
             break;
+
+        case 'pinterest':
+            (function(d){
+                var f = d.getElementsByTagName('SCRIPT')[0], p = d.createElement('SCRIPT');
+                p.type = 'text/javascript';
+                p.async = true;
+                p.src = '//assets.pinterest.com/js/pinit.js';
+                f.parentNode.insertBefore(p, f);
+            }(document));
+            break;
     }
 
     return Socializr;
@@ -81,6 +91,7 @@ Socializr.share = function(platform, url, options) {
             break;
 
         case 'google-plus':
+        case 'pinterest':
         case 'twitter':
             var screenWidth    = window.innerWidth ? window.innerWidth : document.documentElement.clientWidth ? document.documentElement.clientWidth : screen.width,
                 screenHeight   = window.innerHeight ? window.innerHeight : document.documentElement.clientHeight ? document.documentElement.clientHeight : screen.height,
@@ -88,13 +99,13 @@ Socializr.share = function(platform, url, options) {
                 dualScreenTop  = window.screenTop != undefined ? window.screenTop : screen.top;
 
             var width     = screenWidth >= 650 ? 650 : screenWidth / 2,
-                height    = platform === 'twitter' ? 253 : 600, // Hard coded, set by Twitter UI
                 left      = ((screenWidth / 2) - (width / 2)) + dualScreenLeft,
-                top       = ((screenHeight / 2) - (height / 2)) + dualScreenTop,
-                winParams = 'status=1, menubar=no, toolbar=no, scrollbars=yes, top=' + top + ', left=' + left + ', width=' + width + ', height=' + height;
+                top       = ((screenHeight / 2) - 300) + dualScreenTop,
+                winParams = 'status=1, menubar=no, toolbar=no, scrollbars=yes, top=' + top + ', left=' + left + ', width=' + width + ', height=';
 
             if (platform === 'twitter') {
                 url = 'https://twitter.com/intent/tweet?url=' + url;
+                winParams += 253;
 
                 if(typeof options.twitter !== 'undefined') {
                     if(typeof options.twitter.via !== 'undefined') {
@@ -113,6 +124,19 @@ Socializr.share = function(platform, url, options) {
             } else if (platform === 'google-plus') {
                 url = 'https://plus.google.com/share?url=' + url;
                 url += '&h1=' + Socializr.lang.replace('_','-');
+                winParams += 600;
+
+            } else if (platform === 'pinterest') {
+                url = 'http://pinterest.com/pin/create/button/?url=' + url;
+                winParams += 520;
+
+                if(typeof options.pinterest.media !== 'undefined') {
+                    url += '&media=' + options.pinterest.media;
+                }
+
+                if(typeof options.pinterest.description !== 'undefined') {
+                    url += '&description=' + options.pinterest.description;
+                }
             }
 
             var newWindow = window.open(url, platform, winParams);
